9d9153f27cdc880801e70d8ca22ba51161151551
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-09-18 10:44:27 -0230

    Updated Date.java and TestDate.java

--- Problem_3/TestTime.java
+++ Problem_3/TestTime.java
@@ -1,3 +1,42 @@
 public class TestTime {
-    
+    public static void main(String[] args) {
+        Time t1 = new Time(12, 36, 40);
+        Time t2 = new Time(00, 00, 00);
+
+        // Display initial times
+        System.out.println("\nInitial Time: " + t1.getHour() + ":" + t1.getMinute() + ":" + t1.getSecond());
+        System.out.println("Initial Time: " + t2.getHour() + ":" + t2.getMinute() + ":" + t2.getSecond());
+
+        // Display initial times using toString()
+        System.out.println("\nInitial time t1 using toString(): " + t1.toString());
+        System.out.println("Initial time t2 using toString(): " + t2.toString());
+
+        // Display time t1 using setTime
+        t1.setTime(21, 10, 15);
+        System.out.println("\nTime t1 is set to: " + t1.getHour() + ":" + t1.getMinute() + ":" + t1.getSecond());
+
+        // Display time t2 using setTime
+        t2.setTime(10, 20, 25);
+        System.out.println("Time t2 is set to: " + t2.getHour() + ":" + t2.getMinute() + ":" + t2.getSecond());
+
+        // Display time t1 and t2 using setters and toString
+        t1.setHour(21);
+        t1.setMinute(10);
+        t1.setSecond(15);
+        System.out.println("\nSet time t1 using toString(): " + t1.toString());
+
+        
+        t2.setHour(10);
+        t2.setMinute(20);
+        t2.setSecond(25);
+        System.out.println("Set time t2 using toString(): " + t2.toString());
+
+        //  Calling nextSecond() for t1
+        t1.nextSecond();
+        System.out.println("\nThe time t1 after incrementing a second: " + t1.toString());
+
+        //  Calling previousSecond() for t2
+        t2.previousSecond();
+        System.out.println("The time t2 after subtracting a second: " + t2.toString());
+    }
 }
--- Problem_3/Time.java
+++ Problem_3/Time.java
@@ -1,3 +1,61 @@
 public class Time {
-    
+    // Attributes
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    // non-default constructor (Parametrized Constructor)
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    // methods
+    public int getHour() {
+        return this.hour;
+    }
+
+    public int getMinute() {
+        return this.minute;
+    }
+
+    public int getSecond() {
+        return this.second;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public String toString() {
+        return String.format("%02d:%02d:%02d", this.hour, this.minute, this.second);
+    }
+
+    public int nextSecond() {
+        this.second = second + 1;
+        return (this.second);
+    }
+
+    public int previousSecond() {
+        this.second = second - 1;
+        return (this.second);
+    }
 }
+
+
